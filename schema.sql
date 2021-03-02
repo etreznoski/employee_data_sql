b2d3770a91bf128079406f2ed3df42267b02daed
etreznoski <sanchez.elizabethk@gmail.com>
2021-03-01 18:24:40 -0800

    reordered table schema

--- schema.sql
+++ schema.sql
@@ -1,11 +1,23 @@
 -- Create schemas
 
 -- Create tables
-CREATE TABLE IF NOT EXISTS departments
+CREATE TABLE IF NOT EXISTS titles
 (
-    dept_no VARCHAR(10) NOT NULL,
-    dept_name VARCHAR(50),
-    PRIMARY KEY(dept_no)
+    title_id VARCHAR(10) NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    PRIMARY KEY(title_id)
+);
+
+CREATE TABLE IF NOT EXISTS employees
+(
+    employee_no VARCHAR(10) NOT NULL,
+    employee_title VARCHAR(10),
+    birth_date VARCHAR(20),
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50),
+    sex VARCHAR(2) NOT NULL,
+    hire_date VARCHAR(20),
+    PRIMARY KEY(employee_no)
 );
 
 CREATE TABLE IF NOT EXISTS salaries
@@ -13,6 +25,12 @@ CREATE TABLE IF NOT EXISTS salaries
     employee_no VARCHAR(10) NOT NULL,
     salary INTEGER,
     PRIMARY KEY(employee_no)
+);
+CREATE TABLE IF NOT EXISTS departments
+(
+    dept_no VARCHAR(10) NOT NULL,
+    dept_name VARCHAR(50),
+    PRIMARY KEY(dept_no)
 );
 
 CREATE TABLE IF NOT EXISTS dept_managers
@@ -28,26 +46,6 @@ CREATE TABLE IF NOT EXISTS dept_employees
     dept_no VARCHAR(10) NOT NULL,
     PRIMARY KEY(employee_no, dept_no)
 );
-
-CREATE TABLE IF NOT EXISTS employees
-(
-    employee_no VARCHAR(10) NOT NULL,
-    employee_title VARCHAR(10),
-    birth_date VARCHAR(20),
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50),
-    sex VARCHAR(2) NOT NULL,
-    hire_date VARCHAR(20),
-    PRIMARY KEY(employee_no)
-);
-
-CREATE TABLE IF NOT EXISTS titles
-(
-    title_id VARCHAR(10) NOT NULL,
-    title VARCHAR(50) NOT NULL,
-    PRIMARY KEY(title_id)
-);
-
 
 -- Create FKs
 ALTER TABLE dept_employees
