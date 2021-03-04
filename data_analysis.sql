f5eb682c4c0985c80a65be101b82c07570b96989
etreznoski <sanchez.elizabethk@gmail.com>
2021-03-04 14:11:21 -0800

    competed query 7

--- data_analysis.sql
+++ data_analysis.sql
@@ -41,6 +41,12 @@ ON d.dept_no = de.dept_no
 WHERE d.dept_name = 'Sales';
 
 -- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
-
+SELECT e.employee_no, e.last_name, e.first_name, d.dept_name
+FROM dept_employees de 
+JOIN employees e 
+ON e.employee_no = de.employee_no
+JOIN departments d
+ON d.dept_no = de.dept_no
+WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development';
 
 -- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
