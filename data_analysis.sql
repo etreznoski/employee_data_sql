1424d250d8ee8edf4222d7f79e8eb9824a1326e6
etreznoski <sanchez.elizabethk@gmail.com>
2021-03-04 14:16:08 -0800

    competed query 8

--- data_analysis.sql
+++ data_analysis.sql
@@ -50,3 +50,8 @@ ON d.dept_no = de.dept_no
 WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development';
 
 -- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
+SELECT last_name, COUNT(last_name) AS "name_count"
+FROM employees
+GROUP BY last_name
+ORDER BY name_count DESC;
