2fafdfb7d1b3e53419579de5cd43bb35167607c7
etreznoski <sanchez.elizabethk@gmail.com>
2021-03-04 13:55:22 -0800

    completed query 2 and 3

--- data_analysis.sql
+++ data_analysis.sql
@@ -5,10 +5,18 @@ JOIN salaries s
 ON (e.employee_no = s.employee_no);
 
 -- List first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date LIKE '%1986';
 
-
--- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
-
+-- List the manager of each department with the following information: department number,
+--  department name, the manager's employee number, last name, first name.
+SELECT e.first_name, e.last_name, dm.dept_no, d.dept_name
+FROM dept_managers dm
+Join employees e
+ON e.employee_no = dm.employee_no
+JOIN departments d
+ON d.dept_no = dm.dept_no;
 
 -- List the department of each employee with the following information: employee number, last name, first name, and department name.
 
